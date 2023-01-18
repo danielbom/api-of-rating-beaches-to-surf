7602b1843be47a1fae80c0bfc602f72ed34ce3a9
danielbom <daniel.rodrigues.45@hotmail.com>
2023-01-17 21:07:06 -0300

    Update Logger

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,10 +1,16 @@
 import pino from 'pino';
 import config from 'config';
+import pinoPretty from 'pino-pretty';
 
-const loggerEnabled: boolean | string = config.get('App.logger.enabled');
-const Logger = pino({
-  enabled: loggerEnabled === true || loggerEnabled === 'true',
+const isTrue = (value: any) => typeof value === 'string' ? value === 'true' : !!value;
+
+const loggerConfig: pino.LoggerOptions = {
+  enabled: isTrue(config.get('App.logger.enabled')),
   level: config.get('App.logger.level'),
-});
+}
+
+const Logger = isTrue(config.get('App.logger.prettyPrint'))
+  ? pino(loggerConfig, pinoPretty({ colorize: true, ignore: 'hostname' }) as any)
+  : pino(loggerConfig);
 
 export default Logger;
--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -51,7 +51,7 @@ describe('Beaches functional tests', () => {
       expect(response.body).toEqual({
         code: 400,
         error: 'Bad Request',
-        message: 'request.body.lat should be number',
+        message: 'request/body/lat must be number',
       });
     });
 
